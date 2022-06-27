806f454131b7f1e5d0ba0916f56dfb69ba0d005e
Skipp Van Diest <skippvandiest@gmail.com>
2022-06-27 15:41:56 +0200

    Push depuis dev

--- ProjetTest/ProjetTest/Program.cs
+++ ProjetTest/ProjetTest/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Modif au pif pour le merge tkt fais pas gaffe");
+Console.WriteLine("Modif au pif pour le merge depuis dev");
